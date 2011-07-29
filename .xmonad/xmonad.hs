48790f44555b4e037aa3ffbb239e74db31aeb8a1
Brian Middleton <bmiddleton@dhcp-172-18-216-68.mtv.corp.google.com>
2011-07-29 13:00:26 -0700

    Initial commit. Yay!

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,20 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+main = do
+    xmproc <- spawnPipe "/usr/bin/xmobar /home/bmiddleton/.xmobarrc"
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+        , modMask = mod4Mask     -- Rebind Mod to the Windows key
+        } `additionalKeys`
+        [ ((mod4Mask .|. shiftMask, xK_l), spawn "xscreensaver-command -lock")
+        ]
